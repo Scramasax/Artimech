1f4c1c2429e9fa3eea86c72d224f2f49ebfb0520
Scramasax <geolan41@gmail.com>
2019-03-11 23:33:50 -0700

    - basic tool tip and data entry for the configuration state.

--- Assets/Scripts/artiMechStates/aMechArtimechEditor/Visuals/artMainWindowConfigure.cs
+++ Assets/Scripts/artiMechStates/aMechArtimechEditor/Visuals/artMainWindowConfigure.cs
@@ -90,6 +90,8 @@ namespace Artimech
             TitleStyle.normal.textColor = Color.blue;
             TitleStyle.fontSize = 15;
 
+
+
             m_MousePos = Event.current.mousePosition;
             // Color backroundColor = new Color(1, 1, 1, 0.8f);
             Rect rect = new Rect(0, 0, WinRect.width, WinRect.height);
@@ -102,31 +104,65 @@ namespace Artimech
             EditorGUILayout.EndHorizontal();
 
             EditorGUILayout.BeginHorizontal();
-            m_ScrollPos = EditorGUILayout.BeginScrollView(m_ScrollPos, GUILayout.Width(Screen.width-10), GUILayout.Height(Screen.height-100));
+            m_ScrollPos = EditorGUILayout.BeginScrollView(m_ScrollPos, GUILayout.Width(Screen.width - 10), GUILayout.Height(Screen.height - 110));
+
+            int startMargin = 5;
+            int endMargin = 25;
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(startMargin);
+            ArtimechEditor.Inst.RefactorName = EditorGUILayout.TextField("Path/File Statemachine", ArtimechEditor.Inst.RefactorName);
+            GUI.Label(new Rect(1, 1, WinRect.width, 20), new GUIContent("", "This is the path and file name for the statemachine to be copied."));
+            GUILayout.Space(endMargin);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(startMargin);
+            ArtimechEditor.Inst.RefactorName = EditorGUILayout.TextField("Created Script Directory", ArtimechEditor.Inst.RefactorName);
+            GUI.Label(new Rect(1, 20, WinRect.width, 20), new GUIContent("", "This is the root directory the statemachines will be created at."));
+            GUILayout.Space(endMargin);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(startMargin);
+            ArtimechEditor.Inst.RefactorName = EditorGUILayout.TextField("Script Name Prefix", ArtimechEditor.Inst.RefactorName);
+            GUI.Label(new Rect(1, 40, WinRect.width, 20), new GUIContent("", "This is the prefix to use when creating directories and the statemachine name.  The prefix is added to the name of the Unity3d object."));
+            GUILayout.Space(endMargin);
+            EditorGUILayout.EndHorizontal();
+
+            // GUI.Button(new Rect(10, 10, 100, 20), new GUIContent("Click me", "This is the tooltip"));
+
+            // Display the tooltip from the element that has mouseover or keyboard focus
+            //GUI.Label(new Rect(1, 1, 1, 1), GUI.tooltip);
+
+
             //GUILayout.Space(-10);
 
-            for (int i = 0; i < 100; i++)
-            {
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(5);
-                ArtimechEditor.Inst.RefactorName = EditorGUILayout.TextField("Refactor Name", ArtimechEditor.Inst.RefactorName);
-                GUILayout.Space(25);
-                EditorGUILayout.EndHorizontal();
-            }
+            /*       for (int i = 0; i < 100; i++)
+                   {
+                       EditorGUILayout.BeginHorizontal();
+                       GUILayout.Space(5);
+                       ArtimechEditor.Inst.RefactorName = EditorGUILayout.TextField("Refactor Name", ArtimechEditor.Inst.RefactorName);
+                       GUILayout.Space(25);
+                       EditorGUILayout.EndHorizontal();
+                   }*/
 
 
             EditorGUILayout.EndScrollView();
             EditorGUILayout.EndHorizontal();
-
+            GUILayout.Space(5);
             EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(15);
             if (GUILayout.Button("Save"))
             {
                 m_State.OkBool = true;
             }
+            GUILayout.Space(15);
             if (GUILayout.Button("Cancel"))
             {
                 m_State.CancelBool = true;
             }
+            GUILayout.Space(15);
             EditorGUILayout.EndHorizontal();
         }
     }
